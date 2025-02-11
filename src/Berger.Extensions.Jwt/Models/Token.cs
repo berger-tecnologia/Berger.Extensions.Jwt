93bdddbc181735bfe9ff24b6d6912c8d7e667d21
Rafael Cronemberger <rafael@berger.com.br>
2025-02-11 12:04:59 -0300

    Package updates

--- src/Berger.Extensions.Jwt/Models/Token.cs
+++ src/Berger.Extensions.Jwt/Models/Token.cs
@@ -13,6 +13,7 @@ namespace Berger.Extensions.Jwt
         public string Scope { get; set; }
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
+        public string Description { get; set; }
         public DateTime ExpiresOn { get; set; }
     }
 }
